ae371e36ffbe14fa12564e193d64cd1ca9b0030b
OmarAyman1234 <omarayman9124@gmail.com>
2025-02-17 14:00:18 +0200

    Made the server serve React components

--- server/routes/root.js
+++ server/routes/root.js
@@ -3,9 +3,12 @@ const route = express.Router();
 const path = require("path");
 
 route.get("^/$|index(.html)?", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "pages", "index.html"), (err) => {
-    if (err) next(err);
-  });
+  res.sendFile(
+    path.join(__dirname, "..", "..", "client", "dist", "index.html"),
+    (err) => {
+      if (err) next(err);
+    }
+  );
 });
 
 module.exports = route;
--- server/server.js
+++ server/server.js
@@ -21,7 +21,10 @@ app.use(reqLogger);
 
 app.use(cors(corsOptions));
 
+app.use(express.static(path.join(__dirname, "..", "client", "dist")));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", require("./routes/root.js"));
 app.use("/todos", require("./routes/api/todos.js"));
@@ -29,7 +32,8 @@ app.use("/todos", require("./routes/api/todos.js"));
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html"))
-    res.sendFile(path.join(__dirname, "pages", "404.html"));
+    // res.sendFile(path.join(__dirname, "pages", "404.html"));
+    res.send("Not Found!");
   else if (req.accepts("json")) res.json({ error: "404 not found" });
   else res.type("txt").send("404 not found");
 });
