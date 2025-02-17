84afc8fad5b9e2b68e32e977e56ce97aafedb106
OmarAyman1234 <omarayman9124@gmail.com>
2025-02-17 16:27:40 +0200

    App rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import Header from "../components/Header";
 import CreateTodo from "../components/CreateTodo";
 import UnfinishedTodos from "../components/UnfinishedTodos";
 import FinishedTodos from "../components/FinishedTodos";
+import Todos from "../components/Todos";
 function App() {
   return (
     <>
       <Header />
       <CreateTodo />
+      <Todos />
       <UnfinishedTodos />
       <FinishedTodos />
     </>
