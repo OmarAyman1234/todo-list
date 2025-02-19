3c16cb9ec510063d6b53e93ee7d7c3c1b8038089
OmarAyman1234 <omarayman9124@gmail.com>
2025-02-19 17:00:48 +0200

    App.jsx after the TodoContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
 import Header from "../components/Header";
 import CreateTodo from "../components/CreateTodo";
 import Todos from "../components/Todos";
+import { TodoProvider } from "../context/TodoContext";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <>
       <Header />
-      <CreateTodo />
-      <Todos />
+
+      <TodoProvider>
+        <CreateTodo />
+        <Todos />
+      </TodoProvider>
+
+      <Toaster
+        toastOptions={{
+          position: "bottom-right",
+          style: { backgroundColor: "#333", color: "white" },
+        }}
+      />
     </>
   );
 }
