ca4f9133cabdb7c8fa5749b0f0cad7005b94f11f
OmarAyman1234 <omarayman9124@gmail.com>
2025-02-23 21:31:10 +0200

    separated refresh into controller and router.

---
+++ server/controllers/refreshController.js
@@ -0,0 +1,34 @@
+const User = require("../model/User");
+const jwt = require("jsonwebtoken");
+
+const handleRefreshToken = async (req, res) => {
+  if (!req.cookies?.jwt) {
+    return res.sendStatus(401);
+  }
+
+  const refreshToken = req.cookies.jwt;
+
+  const foundUser = await User.findOne({
+    refreshTokens: { $in: [refreshToken] },
+  }).exec();
+
+  if (!foundUser) return res.sendStatus(401);
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || decoded.username !== foundUser.username)
+      return res.sendStatus(403);
+
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+    return res.json({
+      username: foundUser.username,
+      id: foundUser._id,
+      accessToken,
+    });
+  });
+};
+
+module.exports = { handleRefreshToken };
--- server/routes/refresh.js
+++ server/routes/refresh.js
@@ -1,34 +1,7 @@
-const User = require("../model/User");
-const jwt = require("jsonwebtoken");
+const express = require("express");
+const router = express.Router();
+const refreshController = require("../controllers/refreshController");
 
-const handleRefresh = async (req, res) => {
-  if (!req.cookies?.jwt) {
-    return res.sendStatus(401);
-  }
+router.post("/", refreshController.handleRefreshToken);
 
-  const refreshToken = req.cookies.jwt;
-
-  const foundUser = await User.findOne({
-    refreshTokens: { $in: [refreshToken] },
-  }).exec();
-
-  if (!foundUser) return res.sendStatus(401);
-
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-    if (err || decoded.username !== foundUser.username)
-      return res.sendStatus(403);
-
-    const accessToken = jwt.sign(
-      { username: foundUser.username },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "15m" }
-    );
-    return res.json({
-      username: foundUser.username,
-      id: foundUser._id,
-      accessToken,
-    });
-  });
-};
-
-module.exports = handleRefresh;
+module.exports = router;
