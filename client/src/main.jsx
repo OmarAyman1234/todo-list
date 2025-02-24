56e7754c272ca50ade4703bbb056fb2db147ce14
OmarAyman1234 <omarayman9124@gmail.com>
2025-02-24 15:29:06 +0200

    Setting routes via React Router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "../pages/Register.jsx";
+import NotFoundPage from "../pages/NotFoundPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/register", element: <Register /> },
+  { path: "*", element: <NotFoundPage /> },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
