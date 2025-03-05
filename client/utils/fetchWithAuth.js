c4c167b327fc4dbaeeb77342220183a742f298ab
OmarAyman1234 <omarayman9124@gmail.com>
2025-03-05 23:10:54 +0200

    fetchWithAuth function.
    This function handles the expiry of refresh tokens, or refreshing an access token if expired

---
+++ client/utils/fetchWithAuth.js
@@ -0,0 +1,43 @@
+async function fetchWithAuth(url, options = {}, accessToken) {
+  const serverURL = "http://localhost:4444";
+
+  const config = {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: "include",
+  };
+
+  let res = await fetch(url, config);
+  console.log(res);
+
+  if (res.status === 403) {
+    console.log("Reached 403 fetchWithAuth");
+    // Try refreshing the accessToken
+    const refreshResponse = await fetch(serverURL + "/refresh", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    console.log(refreshResponse);
+    if (refreshResponse.status === 401) {
+      // Refresh token expired, login is needed.
+      return refreshResponse;
+    }
+
+    if (refreshResponse.ok) {
+      const data = await refreshResponse.json();
+      const accessToken = data.accessToken;
+
+      // Retry original request
+      config.headers.Authorization = `Bearer ${accessToken}`;
+      res = await fetch(url, config);
+    }
+    console.log(res);
+  }
+  return res;
+}
+
+export default fetchWithAuth;
--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
   console.log(authHeader + "test");
-  if (!authHeader?.startsWith("Bearer")) return res.sendStatus(401);
+  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
 
   const accessToken = authHeader.split(" ")[1];
 
