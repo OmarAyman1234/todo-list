43a2974256277d7d971e8c2510ebe82f988c7ba4
OmarAyman1234 <omarayman9124@gmail.com>
2025-03-08 17:24:52 +0200

    Server would crash if multiple logout done simultaneously

--- server/controllers/logoutController.js
+++ server/controllers/logoutController.js
@@ -4,28 +4,18 @@ const handleLogout = async (req, res) => {
   if (!req.cookies?.jwt) return res.sendStatus(403);
 
   const refreshToken = req.cookies.jwt;
-  const foundUser = await User.findOne({
-    refreshTokens: { $in: [refreshToken] },
-  }).exec();
 
-  if (!foundUser) {
-    res.clearCookie("jwt", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-    });
-    return res.sendStatus(204);
-  }
-
-  const remainingTokens = foundUser.refreshTokens.filter(
-    (token) => token !== refreshToken
-  );
-  foundUser.refreshTokens = remainingTokens;
-  await foundUser.save();
+  await User.findOneAndUpdate(
+    { refreshTokens: refreshToken }, // Find user that has this token in their array
+    { $pull: { refreshTokens: refreshToken } }, // Remove only this specific token
+    { new: true } // Return the updated document
+  ).exec();
 
   res.clearCookie("jwt", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
   });
+
   return res.sendStatus(204);
 };
 
