c694bb2c5d4b6f7019ee59f78696191d38b67f16
OmarAyman1234 <omarayman9124@gmail.com>
2025-03-09 00:11:19 +0200

    Removing StrictMode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -19,7 +18,7 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     <Toaster
       toastOptions={{
         position: "bottom-right",
@@ -32,5 +31,5 @@ createRoot(document.getElementById("root")).render(
         <RouterProvider router={router} />
       </FetchingProvider>
     </AuthProvider>
-  </StrictMode>,
+  </>,
 );
