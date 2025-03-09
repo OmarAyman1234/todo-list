40f331903a5d5a20b81cbff91526cc524127bce9
OmarAyman1234 <omarayman9124@gmail.com>
2025-03-09 17:13:45 +0200

    updated credentials.js

--- server/middleware/credentials.js
+++ server/middleware/credentials.js
@@ -2,9 +2,10 @@ const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin))
-    res.header("Access-Control-Allow-Credentials", true);
-
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+  }
   next();
 };
 
