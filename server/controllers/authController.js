8f273a837a0db2644ce0ffe0e46737d89dfd8b7e
OmarAyman1234 <omarayman9124@gmail.com>
2025-03-09 17:22:09 +0200

    sameSite => none

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -47,6 +47,7 @@ const handleLogin = async (req, res) => {
       maxAge: 1000 * 60 * 60 * 24,
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
+      sameSite: "none",
     });
 
     return res.json({ accessToken });
