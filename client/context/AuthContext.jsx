f330301af357ed3c3702c804b01c2a292d9c4528
OmarAyman1234 <omarayman9124@gmail.com>
2025-04-02 22:17:47 +0200

    authUserData in AuthContext is now managed with a single state

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -3,18 +3,21 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [authUser, setAuthUser] = useState(null);
-  const [userRoles, setUserRoles] = useState(["User"]);
+  const [authUserData, setAuthUserData] = useState({
+    username: "",
+    _id: "",
+    createdAt: "",
+    updatedAt: "",
+    roles: [],
+  });
 
   const [accessToken, setAccessToken] = useState("");
 
   const value = {
     isLoggedIn,
     setIsLoggedIn,
-    authUser,
-    setAuthUser,
-    userRoles,
-    setUserRoles,
+    authUserData,
+    setAuthUserData,
     accessToken,
     setAccessToken,
   };
